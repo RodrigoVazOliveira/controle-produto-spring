9abb39dd5068dcf01f3aefbbd6a3f242e9674cf0
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-04 16:32:26 -0300

    Criar modelo de produto

---
+++ src/main/java/br/dev/rvz/controleproduto/models/Produto.java
@@ -0,0 +1,43 @@
+package br.dev.rvz.controleproduto.models;
+
+public class Produto {
+    private String nome;
+    private String descricacao;
+    private Integer quantidade;
+    private Double preco;
+
+    public Produto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricacao() {
+        return descricacao;
+    }
+
+    public void setDescricacao(String descricacao) {
+        this.descricacao = descricacao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
