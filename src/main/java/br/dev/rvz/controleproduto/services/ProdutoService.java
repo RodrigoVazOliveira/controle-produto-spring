fba2cfad8e61d8f5937b19c1eded8bf711fe143a
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-04 16:41:24 -0300

    Criar Serviço de produto e método para cadastrar um novo produto

---
+++ src/main/java/br/dev/rvz/controleproduto/services/ProdutoService.java
@@ -0,0 +1,35 @@
+package br.dev.rvz.controleproduto.services;
+
+import br.dev.rvz.controleproduto.models.Produto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProdutoService {
+    List<Produto> produtos = new ArrayList<>();
+
+    public void cadastrar(Produto produto) {
+        Produto produtoAjustar = verificarSeProdutoExiste(produto);
+        if (produtoAjustar != null) {
+            ajustarProduto(produtoAjustar, produto);
+        } else {
+            produtos.add(produto);
+        }
+    }
+
+    private void ajustarProduto(Produto produto, Produto novoPoduto) {
+        produto.setQuantidade(produto.getQuantidade() + novoPoduto.getQuantidade());
+    }
+
+    private Produto verificarSeProdutoExiste(Produto produto) {
+        for (Produto p : produtos) {
+            if (p.getNome().equalsIgnoreCase(produto.getNome())) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+}
