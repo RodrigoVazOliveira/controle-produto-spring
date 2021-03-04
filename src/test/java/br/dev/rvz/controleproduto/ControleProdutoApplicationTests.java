068c0e218f8af79710dc6d1bb388a4c006394641
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-04 16:31:00 -0300

    Criar projeto com spring boot

---
+++ src/test/java/br/dev/rvz/controleproduto/ControleProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rvz.controleproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
